4ec91ce96599ac86a4b315924528cd8b1e5171aa
JT <foosyou@gmail.com>
2020-05-28 09:38:15 -0500

    docstring

--- nornir-netbox-demo.py
+++ nornir-netbox-demo.py
@@ -1,3 +1,70 @@
+"""
+nornir-netbox-demo.py
+
+This script uses Netbox as a Source of Truth for Nornir to automate provisioning, deployment and testing of a demo WAN.
+
+Devices must be prepopulated in Netbox with a Primary IP address for management.
+
+Script overview:
+
+* Initialize Nornir/Netbox
+
+* Enable SCP
+
+* Get config variables from Netbox
+
+* Render config files
+
+* Apply Layer 3 configs
+
+* Validate Layer 3 connectivity
+
+* Apply BGP configs
+
+* Validate BGP adjacencies
+
+* Update Netbox data
+
+* Disable SCP / save configs
+
+Sample Config Context provisioning data for Netbox:
+
+{
+    "bgp": {
+        "asn": 65511,
+        "neighbors": [
+            {
+                "ipaddr": "172.20.12.2",
+                "remote_asn": 65512
+            },
+            {
+                "ipaddr": "172.20.13.3",
+                "remote_asn": 65513
+            }
+        ],
+        "rid": "1.1.1.1"
+    },
+    "interfaces": {
+        "GigabitEthernet1": {
+            "description": "Uplink to CSR-2",
+            "ipaddr": "172.20.12.1 255.255.255.0",
+            "state": "up"
+        },
+        "GigabitEthernet2": {
+            "description": "Uplink to CSR-3",
+            "ipaddr": "172.20.13.1 255.255.255.0",
+            "state": "up"
+        },
+        "Loopback0": {
+            "description": "Router ID",
+            "ipaddr": "1.1.1.1 255.255.255.255",
+            "state": "up"
+        }
+    }
+}
+"""
+
+
 import re
 from netbox import NetBox
 from nornir import InitNornir
@@ -9,29 +76,6 @@ from nornir.plugins.tasks.networking import napalm_ping
 from nornir.plugins.tasks.networking import napalm_get
 from nornir.plugins.tasks import text
 from pprint import pprint
-
-"""
-Initilize Nornir and Netbox
-
-Enable SCP on a
-
-Pull config vars from Netbox
-
-Render router configs
-
-Push Layer 3 router configs
-
-Validate Layer 3 connectivity
-
-Push Layer 3 router configs
-
-Validate BGP adjacencies
-
-Update Netbox data
-
-Disable SCP and save configs
-
-"""
 
 
 # print formatting function
